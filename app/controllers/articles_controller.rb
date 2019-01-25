e3b5bbb3423fc9f0088df9c75e1e28ceea5901a8
carly <carly@noukod.com>
2019-01-25 17:51:26 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,31 +1,31 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+ 
   # GET /articles
   # GET /articles.json
   def index
     @articles = Article.all
   end
-
+ 
   # GET /articles/1
   # GET /articles/1.json
   def show
   end
-
+ 
   # GET /articles/new
   def new
     @article = Article.new
   end
-
+ 
   # GET /articles/1/edit
   def edit
   end
-
+ 
   # POST /articles
   # POST /articles.json
   def create
     @article = Article.new(article_params)
-
+ 
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
       end
     end
   end
-
+ 
   # PATCH/PUT /articles/1
   # PATCH/PUT /articles/1.json
   def update
@@ -50,7 +50,7 @@ class ArticlesController < ApplicationController
       end
     end
   end
-
+ 
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
@@ -60,13 +60,13 @@ class ArticlesController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+ 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
     end
-
+ 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
       params.require(:article).permit(:title, :body)
